18a51b0b0fdb09368379ebedf8d5bae8bdbae439
lucithub <lucasvelez2013@gmail.com>
2024-12-05 17:01:50 -0300

    Add more pokemons details

--- js/index.js
+++ js/index.js
@@ -10,11 +10,20 @@ function buscarPokemon(){
         peso.textContent = "Peso: "+data.weight;
         let habilidadesCount = data.abilities;
         let habilidades = document.getElementById("habilidadPokemon");
-        habilidades.textContent = "Habilidades: ";
+        habilidades.textContent = "Habilidades => ";
         habilidadesCount.forEach(abilities => {
-            document.createElement("li");
+            document.createElement("p");
             habilidades.append(abilities.ability.name);
             habilidades.append(', ')
+        });
+        let baseExperience = document.getElementById("baseExperiencePokemon");
+        baseExperience.textContent = "Base Experience => "+data.base_experience;
+        let formas = document.getElementById("formasPokemon");
+        formas.textContent = "Formas => ";
+        let formasCount = data.forms;
+        formasCount.forEach(forms => {
+            formas.append(forms.name);
+            formas.append(', ')
         });
     })
     .catch(error => console.log("Error: "+error));
